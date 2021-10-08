4de65470be44ebdf36b2ada4089cb0d9db05a140
Glagny <nikoicardo@gmail.com>
2021-10-08 11:18:31 -0600

    Add a description map to the Carousel Item.

--- client/src/components/layout/CarouselItem.js
+++ client/src/components/layout/CarouselItem.js
@@ -8,6 +8,12 @@ const CarouselItem = ({ home }) => {
         src={'./img/' + home.image}
         alt={home.description}
       />
+      <div
+        className=" d-none d-sm-block carousel-caption mb-4 mx-auto"
+        style={{ backgroundColor: 'rgba(0, 0, 0, 0.7)' }}
+      >
+        <h3>{home.description}</h3>
+      </div>
     </div>
   );
 };
--- client/src/homePageData.js
+++ client/src/homePageData.js
@@ -1,17 +1,19 @@
 export const homePageData = [
   {
     id: 1,
-    description: '',
+    description: 'We make the renting process easy!',
     image: 'Home1.jpg',
   },
   {
     id: 2,
-    description: '',
+    description:
+      "Need help choosing the right rental, don't worry, we have you covered!",
     image: 'Home2.jpg',
   },
   {
     id: 3,
-    description: '',
+    description:
+      'Listing rentals is as simple as giving us a call at 111-111-1111',
     image: 'Home3.jpg',
   },
 ];
