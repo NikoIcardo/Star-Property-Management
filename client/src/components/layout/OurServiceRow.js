00bd24b9bf8bd495826205eb70b60fdafb06ac5d
Glagny <nikoicardo@gmail.com>
2021-10-11 11:35:56 -0600

    Change OurServiceRow Component Layout to be more appealing.

--- client/src/components/layout/OurServiceRow.js
+++ client/src/components/layout/OurServiceRow.js
@@ -1,22 +1,28 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 const OurServiceRow = ({ row }) => {
   return (
-    <div className="row p-4">
-      <div className={`col-4 ${row.alignment === 'right' && 'order-last'}`}>
-        <h3>{row.header}</h3>
-        <img
-          className="img-fluid"
-          src={row.img.path}
-          alt={row.img.alt}
-          style={{ borderRadius: '4px' }}
-        />
+    <Fragment>
+      <div className="row p-4">
+        <div className={`col-12 ${row.alignment === 'right' && 'text-right'}`}>
+          <h3>{row.header}</h3>
+        </div>
       </div>
-      <div className={`col-8 text-center mt-5`}>
-        <p>{row.text}</p>
+      <div className="row">
+        <div className={`col-4 ${row.alignment === 'right' && 'order-last'}`}>
+          <img
+            className="img-fluid"
+            src={row.img.path}
+            alt={row.img.alt}
+            style={{ borderRadius: '4px' }}
+          />
+        </div>
+        <div className={`col-8 text-center mt-5`}>
+          <p>{row.text}</p>
+        </div>
+        <hr />
       </div>
-      <hr />
-    </div>
+    </Fragment>
   );
 };
 
