0bafbb547a64aaa4e0b729722987883e109fa8db
Glagny <nikoicardo@gmail.com>
2021-10-12 08:28:34 -0600

    Add Property Card Hover Effect.

--- client/src/components/layout/PropertyCard.js
+++ client/src/components/layout/PropertyCard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import './PropertyCard.css';
+
 const PropertyCard = ({ property }) => {
   return (
-    <div className="col-12 col-md-6 col-lg-4 mb-4 d-flex align-items-stretch propertyCard">
+    <div className="col-12 col-md-6 col-lg-4 mb-4 d-flex align-items-stretch propertyCard overlay">
       <div className="card text-center">
         <img
-          className="card-img-top img propertyPhoto"
+          className="img-fluid card-img-top propertyPhoto"
           src={property.img[0].path}
           alt={property.img[0].alt}
         />
@@ -14,6 +16,12 @@ const PropertyCard = ({ property }) => {
         </div>
         <div className="card-body mt-0" style={{ height: '220px' }}>
           <p className="card-text">{property.description}</p>
+        </div>
+
+        <div className="hidden">
+          <h2 className="align-middle mt-5">
+            Interested in this Property? Click for More Info!
+          </h2>
         </div>
       </div>
     </div>
